7a78d90c953a31cdbec65a3a370105397ec4311c
Dimi3y <di0287@ya.ru>
2024-11-20 11:07:23 +0500

    homework-9 - 4

--- src/app/service/toast.service.ts
+++ src/app/service/toast.service.ts
@@ -8,16 +8,13 @@ export class ToastService {
   toastsL:BehaviorSubject<string[]> = new BehaviorSubject<string[]>([])
 
   show(message: string, duration: number = 1500) {
-    let val: string[] = this.toastsL.getValue()
-    val.push(message)
-    this.toastsL.next(val)
+    this.toastsL.next([...this.toastsL.getValue(), message])
     setTimeout(() => {
       this.remove(message)
     }, duration)
   }
 
   remove(message: string) {
-    const val: string[] = this.toastsL.getValue()
-    this.toastsL.next(val.filter((t: string) => t !== message))
+    this.toastsL.next(this.toastsL.getValue().filter((t: string) => t !== message))
   }
 }
